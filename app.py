f84597a53f17e748bcd3a0309625b19a0bf27622
Mohamed Rafiq S <mohamedrafiqs@dhcp-9-239-4-58.c4p-in.ibmmobiledemo.com>
2022-12-02 21:20:32 +0530

    Have made some changes

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import requests
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 app = Flask(__name__)
 
+url = "https://google.com"
+resp = requests.get(url, headers=headers)
+
+print(resp.status_code)
 
 @app.route('/')
 def index():
@@ -20,13 +24,11 @@ def hello():
 
    if name:
        print('Request for hello page received with name=%s' % name)
-       url = "https://google.com"
-       resp = requests.get(url)
-       print(resp.status_code)
        return render_template('hello.html', name = name)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
+
 
 
 if __name__ == '__main__':
