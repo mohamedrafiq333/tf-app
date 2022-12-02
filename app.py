7f40efa37ffc625dc56b52f4a9fbb24627a34e57
Mohamed Rafiq S <mohamedrafiqs@dhcp-9-239-4-58.c4p-in.ibmmobiledemo.com>
2022-12-02 21:04:09 +0530

    Have made some changes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
 def hello():
